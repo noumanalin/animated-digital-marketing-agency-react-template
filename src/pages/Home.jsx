e361b3ecfa03ab26499dc3fecc4a34dfe59e3265
naumanalin <naumanalin865@gmail.com>
2025-08-08 15:59:43 +0500

    Semi-Final V.0.0.1 add:404 page, Go To Top Button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,6 @@ import Hero from '../components/Hero'
 import WhoWeAre from '../components/WhoWeAre'
 import Reviews from '../components/Reviews' 
 import Services from '../components/Services'
-import Footer from '../components/Footer'
 import FeaturedWork from '../components/FeaturedWork'
 import FaqComponent from '../components/faqs/FaqComponent' 
 import Journal from '../components/journal/Journal'
@@ -21,7 +20,7 @@ const Home = () => {
       <FaqComponent/> 
       <Journal/>
     </main>
-    <Footer />
+    
     </>
   )
 }
